f0b8a9f8ef2ab483aefc31ab1830cbdd7f3c0e36
Alexander Diebler <alexander.diebler@barzahlen.de>
2015-04-17 12:55:43 +0200

    added mobile selection template, fixed cancellation

---
+++ src/xtAdmin/zi.cancel.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Barzahlen Payment Module (xt:Commerce 4)
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ *
+ * @copyright   Copyright (c) 2012 Zerebro Internet GmbH (http://www.barzahlen.de)
+ * @author      Alexander Diebler
+ * @license     http://opensource.org/licenses/GPL-3.0  GNU General Public License, version 3 (GPL-3.0)
+ */
+
+require_once '../xtCore/main.php';
+require_once _SRV_WEBROOT . 'plugins/zi_barzahlen/classes/class.cancel.php';
+
+if(barzahlen_cancel::tryCancel() == true) {
+  header("HTTP/1.1 200 OK");
+  header("Status: 200 OK");
+}
+else {
+  header("HTTP/1.1 400 Bad Request");
+  header("Status: 400 Bad Request");
+}
+?>
